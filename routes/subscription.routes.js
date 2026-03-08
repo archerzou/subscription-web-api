a6089f3524658e49659f775242487faaaa7ec595
Zou Zhefeng <archer.zou84@gmail.com>
2026-03-08 19:30:51 +1300

    first commit

---
+++ routes/subscription.routes.js
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import authorize from '../middlewares/auth.middleware.js'
+import {
+    createSubscription,
+    getUserSubscriptions,
+    getAllSubscriptions,
+    getSubscriptionById,
+} from '../controllers/subscription.controller.js'
+
+const subscriptionRouter = Router();
+
+subscriptionRouter.get('/', getAllSubscriptions);
+
+subscriptionRouter.get('/:id', getSubscriptionById);
+
+subscriptionRouter.post('/', authorize, createSubscription);
+
+subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
+
+subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
+
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
+
+subscriptionRouter.put('/:id/cancel', (req, res) => res.send({ title: 'CANCEL subscription' }));
+
+subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({ title: 'GET upcoming renewals' }));
+
+export default subscriptionRouter;
